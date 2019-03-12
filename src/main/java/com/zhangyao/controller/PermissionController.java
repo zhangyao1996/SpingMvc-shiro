53099f8d9be8abf9725e49bf22ef36137297fb0d
Administrator <Administrator@172.22.60.60>
2019-03-12 17:59:40 +0800

    ..

--- src/main/java/com/zhangyao/controller/PermissionController.java
+++ src/main/java/com/zhangyao/controller/PermissionController.java
@@ -1,13 +1,17 @@
 package com.zhangyao.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -85,5 +89,64 @@ public class PermissionController {
         }
     }
 	
+    
+    /**
+     * 更新角色-权限信息，包含以下参数：
+     * <p>
+     * id      当前权限的id
+     * ids     当前角色-权限的id集合
+     * parents 当前角色-权限的是否是父节点的parent集合
+     *
+     * <p>
+     * 之前在User层面上，我们要对其进行角色的更新，也就是说在User用户层面上更新权限，只需要维护用户-角色表数据。
+     * 而在角色层面上，只需要维护角色-权限表数据。此处在权限层面上，也是维护角色-权限表数据
+     * <p>
+     * 误区：始终要记得，同一个角色拥有不同权限的情况是不存在的，如果两个用户拥有不同的权限，那么他们一定拥有了不同的角色
+     *
+     * @return
+     * @Param dataMap 以上参数数据的Map集合
+     *
+     * <p>
+     * 为什么要用Map接收？
+     * 如果想要传递对象(如数组)参数，ajax就必须发送post请求，而post请求传递对象参数就要用JSON.stringify()格式化数据为JSON字符串才能传递；
+     * 一旦使用了JSON.stringify()格式化数据，传递的就是JSON字符串，后端必须使用String类型接收，而我们传递的数据中包含了普通变量、数组等多种数据，所以使用使用Map接收，并指定泛型是String类型
+     *
+     * <p>
+     * 前端传递进来的参数在Map中封装的数据结构类似：
+     * {0:{key: id, value: {....}}, 1:{key: ids, value: {...}}, 2:{key: parents, value: {...}}}
+     */
+    @ResponseBody
+    @RequestMapping(value = "/updateRolesPermissions", method = RequestMethod.POST)
+    public Map updateRolesPermissions(@RequestBody Map<String, Object> dataMap) {
+    	Map map=new HashMap<>();
+        try {
+            Long id = Long.valueOf((String) dataMap.get("id")); //当前操作权限的id
+            ArrayList ids = (ArrayList) dataMap.get("ids"); //当前权限更新的关联角色的id集合体
+            ArrayList parents = (ArrayList) dataMap.get("parents"); //当前更新的角色权限parent信息
+
+            // 更新权限与角色关系即需要维护角色-权限表，前端传来了什么数据？ 1、权限id；2、被选中的角色Id集合。
+            // 如何更新权限关联的角色？我们常会想到，根据表的主键update呀，但是，因为页面上展示的数据是后端构建的ZTree实体类JSON数据，其中并不包含表的主键值
+            // 所以，这里就采取一种比较极端的方式，先删除此权限所有关联的角色id，再依次关联此权限当前更新的角色id
+
+            //先删除此权限关联的所有角色id
+            permissionService.deleteAllPermissionsRoles(id);
+
+            //再依次关联此去权限更新的角色id
+            for (int i = 0; i < ids.size(); i++) {
+                if (!(boolean) parents.get(i)) {
+                    //不是父节点才进行关联，因为父节点是角色，子节点才是权限
+                    permissionService.correlationRoles(id, Long.valueOf(String.valueOf(ids.get(i))));
+                }
+            }
+            System.out.println(dataMap);
+            map.put("success", "更新成功");
+            return map;
+        } catch (Exception e) {
+            e.printStackTrace();
+            map.put("false", "更新失败");
+            return map;
+        }
+    }
+
 
 }
